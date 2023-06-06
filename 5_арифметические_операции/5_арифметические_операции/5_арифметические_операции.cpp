4fc02f69eb8c9003dc9988b6f02cbc0980e6d98d
Igordeveloperr <g.bikmetow@gmail.com>
2023-06-06 21:52:23 +0300

    арифметика момэнт

---
+++ 5_арифметические_операции/5_арифметические_операции/5_арифметические_операции.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    int a = 5;
+    cout << a << endl;
+    a = -a; // унарный минус
+    cout << a << endl;
+
+    int b = 10, c = 17, res;
+    res = b + c;
+    cout << res << endl;
+
+    res = b - c;
+    cout << res << endl;
+
+    res = b * c;
+    cout << res << endl;
+
+    double div = double(b) / double(c);
+    cout << div << endl;
+
+    res = b % c;
+    cout << res << endl;
+}
