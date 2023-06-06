8a5db8032a275ea5d212152dbb1f6a7a6f7ff10b
Igordeveloperr <g.bikmetow@gmail.com>
2023-06-06 22:03:32 +0300

    логично

---
+++ 6_логические_операции/6_логические_операции/6_логические_операции.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	bool a = false, b = true;
+
+	cout << (45 > 90) << endl;
+	cout << (!a || b) << endl;
+	cout << (a == b) << endl;
+	cout << (a != b) << endl;
+	cout << (a && b) << endl;
+	cout << !b << endl;
+}
