287ee2b4404cdaa0008eb69ea912d154d770cb21
Igordeveloperr <g.bikmetow@gmail.com>
2023-06-06 20:34:26 +0300

    первая приложуха шо

---
+++ 1_firstApp/1_firstApp/1_firstApp.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    int a = 0;
+    int b = 0;
+
+    cout << "Введите a: ";
+    cin >> a;
+
+    cout << "Введите b: ";
+    cin >> b;
+
+    cout << "Сумма: " << a + b;
+}
