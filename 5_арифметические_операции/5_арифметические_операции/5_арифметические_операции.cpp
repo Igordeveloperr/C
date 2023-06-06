c5c49bc541ba94ddd058f3d5d1693f5fc7e70cf6
Igordeveloperr <g.bikmetow@gmail.com>
2023-06-06 21:55:58 +0300

    арифметика момэнт

--- 5_арифметические_операции/5_арифметические_операции/5_арифметические_операции.cpp
+++ 5_арифметические_операции/5_арифметические_операции/5_арифметические_операции.cpp
@@ -10,6 +10,7 @@ int main()
     a = -a; // унарный минус
     cout << a << endl;
 
+    // ну тут классика
     int b = 10, c = 17, res;
     res = b + c;
     cout << res << endl;
@@ -25,4 +26,12 @@ int main()
 
     res = b % c;
     cout << res << endl;
+
+    // инкремент и дикремент
+    int i = 9;
+    i--;
+    i -= 2;
+    i++;
+    i += 10;
+    cout << i << endl;
 }
