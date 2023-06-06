f791c85a5199192146afbed00e99aa33c5ce2bb1
Igordeveloperr <g.bikmetow@gmail.com>
2023-06-06 22:12:03 +0300

    минус условия

---
+++ 7_условия/7_условия/7_условия.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+    
+	int a , b;
+
+	cout << "a: ";
+	cin >> a;
+	cout << "b: ";
+	cin >> b;
+
+	if ((a > b) && (a <= 100)) 
+	{
+		cout << "Я ОПРЕДЕЛЕННО ХОЧУ ПИЦЦЫ!!!!";
+	}
+	else if ((a < b) || (b > 0)) 
+	{
+		cout << "Я ВЫПОЛЗ";
+	}
+	else 
+	{
+		cout << "ВСЕ УДАЛЯЙ";
+	}
+}
