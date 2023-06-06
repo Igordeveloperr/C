fef8514c91172a0c2e02994723c3ceb9eb497fad
Igordeveloperr <g.bikmetow@gmail.com>
2023-06-06 21:38:51 +0300

    ну константы, не добавить, не убавить

---
+++ 4_константы/4_константы/4_константы.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+using namespace std;
+
+const double PI = 3.14;
+const char NEXT_LINE = '\n';
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    double sinA = 0.5;
+    cout << sinA * PI << endl;
+
+    cout << "Это конец шо" << NEXT_LINE << "БАЗА";
+}
+
