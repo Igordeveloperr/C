8df816433891857fad6ad1be2470d0b12c347e70
Igordeveloperr <g.bikmetow@gmail.com>
2023-06-25 10:28:47 +0300

    повторил switch

---
+++ 8_switchCase/8_switchCase/8_switchCase.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+    switch (a) 
+    {
+        case 20:
+            cout << "a = 20";
+            break;
+        case 40:
+            cout << "a = 40";
+            break;
+        default:
+            a = -1;
+            cout << "not valid data";
+            break;
+    }
+}
